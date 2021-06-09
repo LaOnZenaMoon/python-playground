8cc94e8fd58713ab8918de941b0a4f0ed10f4ba1
junlee <laonzenamoon@gmail.com>
2021-06-09 11:42:07 +0900

    feat: Chaning 기법을 활용한 Collision 방지 HashTable 구현

--- dataStructure/hashTable/HashTableMadeByList.py
+++ dataStructure/hashTable/HashTableMadeByList.py
@@ -17,6 +17,36 @@ def readData(data):
     hashAddress = hashFunction(getKey(data))
     return hash_table[hashAddress]
 
+hash_table2 = list([0 for i in range(8)])
+
+def saveDataUsingChaining(data, value):
+    index_key = getKey(data)
+    hash_address = hashFunction(index_key)
+
+    if hash_table2[hash_address] != 0:
+        for index in range(hash_address, len(hash_table2)):
+            if hash_table2[index] == 0:
+                hash_table2[index] = [index_key, value]
+                return
+            elif hash_table2[index][0] == index_key:
+                hash_table2[index][1] = value
+                return
+    else:
+        hash_table2[hash_address] = [index_key, value]
+
+def readDataUsingChaining(data):
+    index_key = getKey(data)
+    hash_address = hashFunction(index_key)
+
+    if hash_table2[hash_address] != 0:
+        for index in range(hash_address, len(hash_table2)):
+            if hash_table2[index] == 0:
+                return None
+            elif hash_table2[index][0] == index_key:
+                return hash_table2[index][1]
+    else:
+        return None
+
 
 print(hash_table)
 
@@ -25,3 +55,16 @@ saveData('andy', '0120123123')
 print(readData('andy'))
 
 print(hash_table)
+
+
+print (hash('Dd') % 8)
+print (hash('dc') % 8)
+
+saveDataUsingChaining('Dd', '01012341234')
+saveDataUsingChaining('dc', '01011111')
+
+print(hash_table2)
+
+print(readDataUsingChaining('Dd'))
+
+print(hash_table2)
