0a1e7e44e2125f4a429bc4607687bc478342e9c7
junlee <laonzenamoon@gmail.com>
2021-06-14 09:55:41 +0900

    feat: LinkedListManagement.py 구현
    - 추가
    - 삭제
    - 노드 출력

--- dataStructure/list/LinkedListNode.py
+++ dataStructure/list/LinkedListNode.py
@@ -1,35 +1,4 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
-
-
-def add(data):
-    node = head
-
-    while node.next:
-        node = node.next
-
-    node.next = Node(data)
-
-
-node1 = Node(1)
-node2 = Node(2)
-node1.next = node2
-head = node1
-
-for index in range(3, 11):
-    add(index)
-
-print(node1)
-
-node = head
-while node.next:
-    print(node.data)
-    node = node.next
-
-print(node)
