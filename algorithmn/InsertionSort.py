c4e3fcaa532ca3a7eb3552efeb9b4397bcbe0877
junlee <laonzenamoon@gmail.com>
2021-06-21 08:58:30 +0900

    feat: InsertionSort 구현

---
+++ algorithmn/InsertionSort.py
@@ -0,0 +1,16 @@
+def insertionSort(data):
+    for index in range(len(data) - 1):
+        for index2 in range(index + 1, 0, -1):
+            if data[index2] < data[index2 - 1]:
+                data[index2], data[index2 - 1] = data[index2 - 1], data[index2]
+            else:
+                break
+
+    return data
+
+
+import random
+
+data_list = random.sample(range(100), 50)
+print(data_list)
+print(insertionSort(data_list))
